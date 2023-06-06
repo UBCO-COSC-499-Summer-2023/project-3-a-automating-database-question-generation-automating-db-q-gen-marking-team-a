a0609fb5b4dcf40c58667c08c6c92f5d0b1177e7
nishant-sr <nishant.srinivasan236@gmail.com>
2023-06-06 13:18:27 -0700

    got element to render question text

--- databaseCourse/elements/pl-ddl-element/pl-ddl-element.py
+++ databaseCourse/elements/pl-ddl-element/pl-ddl-element.py
@@ -13,9 +13,10 @@ def render(element_html, data):
         }
         with open('ddl-element.mustache', 'r', encoding='utf-8') as f:
             html = chevron.render(f, html_params).strip()
-    """    
-    print(element)
+    """
     
-    html = chevron.render('Hello, {{ mustache }}!', {'mustache': 'World'})
+    z = pl.inner_html(element[0])
+    
+    html = chevron.render('{{ mustache }}!', {'mustache': z})
     
     return html
