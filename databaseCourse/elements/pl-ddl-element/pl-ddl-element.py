ec9eb2ecc69fb5d3d740a5a2e80cb8d891db692b
azipis <andrei_zipis@hotmail.com>
2023-06-06 13:19:42 -0700

    Rendering the mustache file.

--- databaseCourse/elements/pl-ddl-element/pl-ddl-element.py
+++ databaseCourse/elements/pl-ddl-element/pl-ddl-element.py
@@ -5,17 +5,15 @@ import prairielearn as pl
 
 def render(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
-    
-    """
+
     if data['panel'] == 'question':        
         html_params = {
-            'question_data': data['params']['question_data'],
+            "question": True,
         }
-        with open('ddl-element.mustache', 'r', encoding='utf-8') as f:
+        with open('pl-ddl-element.mustache', 'r', encoding='utf-8') as f:
             html = chevron.render(f, html_params).strip()
-    """    
-    print(element)
+
     
-    html = chevron.render('Hello, {{ mustache }}!', {'mustache': 'World'})
+    #html = chevron.render('Hello, {{ mustache }}!', {'mustache': 'World'})
     
     return html
