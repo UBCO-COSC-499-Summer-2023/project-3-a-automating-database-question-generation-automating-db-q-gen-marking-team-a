d3242975341d9b8a3d67162697da6d060ebfa7bf
nishant-sr <nishant.srinivasan236@gmail.com>
2023-06-06 18:05:03 -0700

    hooked up render.js file, maybe we can setup SQL input in there

--- databaseCourse/elements/pl-ddl-element/pl-ddl-element.py
+++ databaseCourse/elements/pl-ddl-element/pl-ddl-element.py
@@ -5,16 +5,7 @@ import prairielearn as pl
 
 def render(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
-    
-    """
-    if data['panel'] == 'question':        
-        html_params = {
-            'question_data': data['params']['question_data'],
-        }
-        with open('ddl-element.mustache', 'r', encoding='utf-8') as f:
-            html = chevron.render(f, html_params).strip()
-    """
-    
+     
     z = pl.inner_html(element[0])
     html_params = {
         'questionText' : z
