890a8359a069559e7be2c3d192a3fc2c6d163689
azipis <andrei_zipis@hotmail.com>
2023-06-07 22:37:15 -0700

    Added comments to ddl-element.py. Attempted to get sql.js working, did not succeed.

--- databaseCourse/elements/pl-ddl-element/pl-ddl-element.py
+++ databaseCourse/elements/pl-ddl-element/pl-ddl-element.py
@@ -3,15 +3,39 @@ import chevron
 import lxml.html
 import prairielearn as pl
 
+
 def render(element_html, data):
+    '''
+    Renders the question, submission and answer into PrairieLearn.
+    This function is called by PrairieLearn during the page load.
+    
+    Keyword arguments:
+    :param element_html: The element HTML
+    :param data: The data
+    :return: The HTML you want rendered
+    '''
+    
+    
     element = lxml.html.fragment_fromstring(element_html)
      
-    z = pl.inner_html(element[0])
-    html_params = {
-        'questionText' : z
-    }
+    # This renders the question into PL
+    if data['panel'] == 'question':  
+        z = pl.inner_html(element[0])
+        html_params = {
+            'questionText' : z
+        }
     
-    with open('pl-ddl-element.mustache', 'r') as f:
-        html = chevron.render(f, html_params)
+        with open('pl-ddl-element.mustache', 'r') as f:
+            html = chevron.render(f, html_params)
+        
+    # This renders the users submitted answer into the "Submitted answer" box in PL
+    elif data['panel'] == 'submission':
+        html = "Submitted string"
+    
+    # This renders the correct answer into the "Correct answer" box in PL
+    # This will not be displayed on the student page unless a showCorrectAnswer: True 
+    # is specified in the info.json file.
+    elif data['panel'] == 'answer':
+        html = "Answer string"
     
     return html
