6715997fc7d5ada2057d3dae5eb3bd36c21b9585
nishant-sr <nishant.srinivasan236@gmail.com>
2023-06-16 14:48:22 -0700

    cleaned up renderer.js files a little bit

--- databaseCourse/elements/pl-ddl-element/renderer.js
+++ databaseCourse/elements/pl-ddl-element/renderer.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     /*
     //
-    // Initializing sql.js and wasm
+    // Initializing sql.js and wasm--------------------------------------------------------------------------------------------
     //
     */
 
@@ -18,10 +18,7 @@ $(document).ready(function () {
     var db;
     // Initialize the db with sql.js
     initSqlJs(config).then(function (SQL) {
-        console.log("sql.js initialized 🎉");
-
         db = new SQL.Database();
-
     });
 
     var execBtn = $("#execute");
@@ -52,6 +49,12 @@ $(document).ready(function () {
         }
     });
 
+    /*
+    //
+    // Functions regarding the rendering of the Database Schema -------------------------------------------------------------------
+    // modeled after the schema & dropdowns visualization found in autoEr
+    */
+
     // Function that shows DB schema/tables
     function showDBTables() {
 
@@ -72,53 +75,49 @@ $(document).ready(function () {
 
     }
 
-    // Function that creates tables that showcase schema
-    function createSchemaTables(results, tableName) {
+    // Function that creates tables that showcase schema fields
+    function createSchemaTables(schemaFields, tableName) {
 
         // if tables have no rows, means table does not exist
-        if (results.length === 0) {
+        if (schemaFields.length === 0) {
             return;
         }
 
-        // let table = $("<table></table>");
-        // let header = $("<thead></thead>");
-        // let headerRow = $("<tr></tr>");
-        // let tableHeader = $(`<th colspan="2">${tableName}</th>`);
-        // headerRow.append(tableHeader);
-        // header.append(headerRow);
-        // table.append(header);
-
-        // for (var i = 0; i < results.length; i++) {
-        //     table.append(createTableRows(results[i].values));
-        // }
-        console.log(results,tableName)
+        // creates html button for the table as well as layout for dropdown
         let schemaView = "<div class='schemaTable'>" 
         + "<button type='button' onClick='openMenu(this)' class='dropbtn' id='btn-" + tableName + "'>" + tableName 
         + "</button> <div class='dropdown-content' id='schema-" + tableName + "'>"
 
-        for (var i = 0; i < results[0].values.length; i++) {
+        // create a row for each field in the table
+        for (var i = 0; i < schemaFields[0].values.length; i++) {
             let field = "<div style='text-align:center;padding:0.2em;border:0.2px solid white;' classname='submenu' id='schema-" 
-            + tableName + "'>" + results[0].values[i][0] 
-            + " , " +  results[0].values[i][1] + "</div>"
-            console.log(i)
+            + tableName + "'>" + schemaFields[0].values[i][0] 
+            + " , " +  schemaFields[0].values[i][1] + "</div>"
             schemaView+=(field)
         }
 
         schemaView += "</div></div>"
         return schemaView
-
     }
 
-    // function to show the modals of the schemas
-    window.openMenu = function(val){
+    /*
+    //
+    // Functions regarding the dropdowns' visbility ---------------------------------------------------------------------------
+    // modeled after the dropdowns found in autoEr
+    */
+
+    // function to show the dropdown of the selected schema
+    window.openMenu = function(tableName){
+        // so that only the dropdown of one schema is open at a time
         closeMenus();
-        console.log(val.id.slice(4))
-        document.getElementById('schema-' + val.id.slice(4)).classList.toggle('show');
+        let schemaDropdownId = 'schema-' + tableName.id.slice(4)
+        document.getElementById(schemaDropdownId).classList.toggle('show');
     }
 
-    // function to close the modals of the schemas, basically hides each field in the modal
+    // function to close any and all dropdowns that are showing
     function closeMenus(){
-        let dropdowns = document.getElementsByClassName('dropdown-content');
+        let allDropDownsClass = 'dropdown-content'
+        let dropdowns = document.getElementsByClassName(allDropDownsClass);
         for(let i = 0; i < dropdowns.length; i++){
             if(dropdowns[i].classList.contains('show')){
                 dropdowns[i].classList.remove('show')
@@ -126,7 +125,7 @@ $(document).ready(function () {
         }
     }
 
-    // if you click outside of a schema name button, the modal goes away
+    // close and and all open dropdowns if the user clicks away from schema table name buttons
     window.onclick = function(e){
         if(!e.target.matches('.dropbtn')){
             closeMenus()
@@ -136,13 +135,12 @@ $(document).ready(function () {
 
     /*
     //
-    // Functions regarding the SQL editor execute button
+    // Functions regarding the SQL editor execute button -------------------------------------------------------------------
     //
     */
 
     // Execute the sql code
     // Create table
-    // TODO: Refactor all code that creates tables using sql.js API
     function execute(sqlCode) {
 
         // clear all previous results
@@ -170,7 +168,6 @@ $(document).ready(function () {
             }
 
         } catch (e) {
-            console.log(e);
             errorElm.append(e);
         }
     }
@@ -234,6 +231,12 @@ $(document).ready(function () {
         }
     }
 
+    /*
+    //
+    // Functions regarding the output table generation -----------------------------------------------------------------------
+    //
+    */
+
     // Function that gets the table name from a SELECT SQL statement
     function getTableName(regex, sqlStatement) {
 
@@ -265,7 +268,6 @@ $(document).ready(function () {
     }
 
     // Function that creates the table header
-
     function createTableHeader(columns) {
 
 
@@ -284,7 +286,6 @@ $(document).ready(function () {
     }
 
     // Function that creates the table rows for a table
-
     function createTableRows(rows) {
         var rowElements = [];
         rows.forEach(function (row) {
@@ -299,83 +300,3 @@ $(document).ready(function () {
     }
 
 });
-
-/*
-//
-// 
-//
-*/
-
-
-
-/* Testing Queries 
-
-
-CREATE TABLE airplane ( id CHAR(10) , model CHAR(20), manufacture_date DATE );
-
-CREATE TABLE dept (
-   deptId integer,
-   deptName varchar(40),
-   deptLocation varchar(50),
-   primary key (deptId)
-);
-
-CREATE TABLE courseDept (
-   deptId integer,
-   cname varchar(40),   
-   primary key (deptId, cname),
-   FOREIGN KEY (cname) REFERENCES course (cname) ON UPDATE CASCADE,
-   FOREIGN KEY (deptId) REFERENCES dept (deptId) ON UPDATE CASCADE
-); 
-
-CREATE TABLE emp (
- eno CHAR(5) NOT NULL,
- ename VARCHAR(30),
- bdate DATE,
- title CHAR(2),			
- salary DECIMAL(9,2),
- supereno CHAR(5),
- dno CHAR(5),
- PRIMARY KEY (eno)
-);
-CREATE TABLE proj (
- pno CHAR(5) NOT NULL,
- pname VARCHAR(40),
- budget DECIMAL(9,2),
- dno  CHAR(5), 
- PRIMARY KEY (pno),
- CONSTRAINT FK_proj_dept FOREIGN KEY (dno) REFERENCES dept(dno) ON DELETE SET NULL);
-
-
-
-DROP TABLE IF EXISTS employees;
-CREATE TABLE employees( id          integer,  name    text,
-                          designation text,     manager integer,
-                          hired_on    date,     salary  integer,
-                          commission  float,    dept    integer);
-
-  INSERT INTO employees VALUES (1,'JOHNSON','ADMIN',6,'1990-12-17',18000,NULL,4);
-  INSERT INTO employees VALUES (2,'HARDING','MANAGER',9,'1998-02-02',52000,300,3);
-  INSERT INTO employees VALUES (3,'TAFT','SALES I',2,'1996-01-02',25000,500,3);
-  INSERT INTO employees VALUES (4,'HOOVER','SALES I',2,'1990-04-02',27000,NULL,3);
-  INSERT INTO employees VALUES (5,'LINCOLN','TECH',6,'1994-06-23',22500,1400,4);
-  INSERT INTO employees VALUES (6,'GARFIELD','MANAGER',9,'1993-05-01',54000,NULL,4);
-  INSERT INTO employees VALUES (7,'POLK','TECH',6,'1997-09-22',25000,NULL,4);
-  INSERT INTO employees VALUES (8,'GRANT','ENGINEER',10,'1997-03-30',32000,NULL,2);
-  INSERT INTO employees VALUES (9,'JACKSON','CEO',NULL,'1990-01-01',75000,NULL,4);
-  INSERT INTO employees VALUES (10,'FILLMORE','MANAGER',9,'1994-08-09',56000,NULL,2);
-  INSERT INTO employees VALUES (11,'ADAMS','ENGINEER',10,'1996-03-15',34000,NULL,2);
-  INSERT INTO employees VALUES (12,'WASHINGTON','ADMIN',6,'1998-04-16',18000,NULL,4);
-  INSERT INTO employees VALUES (13,'MONROE','ENGINEER',10,'2000-12-03',30000,NULL,2);
-  INSERT INTO employees VALUES (14,'ROOSEVELT','CPA',9,'1995-10-12',35000,NULL,1);
-
-SELECT designation,COUNT(*) AS nbr, (AVG(salary)) AS avg_salary FROM employees GROUP BY designation ORDER BY avg_salary DESC;
-SELECT name,hired_on FROM employees ORDER BY hired_on;
-
-
-
-
-
-
-
-*/
