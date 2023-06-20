3ade2fdb050d732f65e5e38d1462f6f23057d168
nishant-sr <nishant.srinivasan236@gmail.com>
2023-06-20 10:30:39 -0700

    grading doesn't seem to work, gonna have to look into it

--- databaseCourse/questions/SQL_Questions/1/server.py
+++ databaseCourse/questions/SQL_Questions/1/server.py
@@ -1,13 +1,18 @@
 textfile = open("../lab3_db.txt","r")
+textfile1 = open("../lab3_ans.txt","r")
 lines = textfile.read().splitlines()
+lines1 = textfile1.read().splitlines()
 textfile.close()
+textfile1.close()
 
 def imports(data):
     pass
     
 def generate(data):
     data["params"]["ddl"] = lines
-    #data["correct_answers"]["SQLEditor"]
+    print(lines1[0])
+    data["params"]["grader"] = "SQLEditor"
+    data["correct_answers"]["SQLEditor"] = """SELECT pname AS product_name, price, inventory, (price * inventory) AS inventoryValue FROM product WHERE price > 20 ORDER BY pname ASC;"""
     pass
     
 def prepare(data):
