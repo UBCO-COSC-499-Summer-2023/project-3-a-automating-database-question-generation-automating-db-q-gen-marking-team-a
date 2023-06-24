2ed723821aab2ca03651c22e518546e77e8839a8
azipis <andrei_zipis@hotmail.com>
2023-06-23 17:11:52 -0700

    Submission and correct answer now showing in correct boxes.

--- databaseCourse/elements/pl-relax-element/pl-relax-element.py
+++ databaseCourse/elements/pl-relax-element/pl-relax-element.py
@@ -2,45 +2,51 @@ import json
 import chevron
 import lxml.html
 import prairielearn as pl
+import RASQLCustomGrader as grader
 
-'''
-def prepare():
-    pass '''
+
+def generate(element_html, data):
+    pass
+    
+
+
+def prepare(element_html, data):
+    data['params']['grader'] = 'RelaXEditor'
+    element = lxml.html.fragment_fromstring(element_html)
+    
+    correctAnswer = pl.inner_html(element[2])
+    data['correct_answers']['RelaXEditor'] = correctAnswer
 
 def render(element_html, data):
-    # Gets the element data from the HTML
+    # # Gets the element data from the HTML
     element = lxml.html.fragment_fromstring(element_html)
-    # Gets each element from the questionHTML
+    # # Gets each element from the questionHTML
+        
     questionText = pl.inner_html(element[0])
     database = pl.inner_html(element[1])
 
 
     submittedAnswer = data['submitted_answers'].get('RelaXEditor','')
-    correctAnswer = data['correct_answers']["RelaXEditor"]
-
-    print("SA", submittedAnswer)
-    print("CA", correctAnswer)
+    correctAnswer = data['correct_answers']['RelaXEditor']
     
     # This renders the question into PL
-    if data['panel'] == 'question':  
+    if data['panel'] == 'question':
         # setting the paramaters
         html_params = {
-            'answer': True,
-            'submission': True,
             'questionText' : questionText,
             'database' : database,
-            'submissionAnswer': submittedAnswer,
-            'correctAnswer': correctAnswer
         }
             # Opens and renders mustache file into the question html
-        with open('pl-relax-element.mustache', 'r') as f:
+        with open('pl-relax-element.mustache', 'r', encoding='utf-8') as f:
             html = chevron.render(f, html_params)
 
+
     elif data['panel'] == 'submission':
-        print("submission:", submittedAnswer)
+  
         html_params = {
+            
             'submission': True,
-            'submissionAnswer': submittedAnswer
+            'submissionAnswer': submittedAnswer,
         }
         
         with open('pl-relax-submission.mustache', 'r', encoding='utf-8') as f:
@@ -50,7 +56,8 @@ def render(element_html, data):
     # This will not be displayed on the student page unless a showCorrectAnswer: True 
     # is specified in the info.json file.
     elif data['panel'] == 'answer':
-        print("correctAnswer:", correctAnswer)
+        
+        #print("correctAnswer:", correctAnswer)
         html_params = {
             'answer': True,
             'correctAnswer': correctAnswer
@@ -60,3 +67,25 @@ def render(element_html, data):
 
 
     return html
+
+def parse(element_html, data):
+    pass
+
+def grade(element_html, data):
+    # Grades the student's submission
+    studentScore = grader.customGrader(data)
+    
+    # Places the student's score and other feedback into data.
+    # Score cannot be directly modified in the element folder,
+    # rather it must be placed within partial scores.
+    # Updating final score is done automatically by PrairieLearn
+    # based upon the partial scores.
+    data['partial_scores']['RelaXEditor'] = {
+        'score': studentScore,
+        'weight': 1,
+        'feedback': "",
+        'marker_feedback': ""
+    }
+
+def test(element_html, data):
+    pass
