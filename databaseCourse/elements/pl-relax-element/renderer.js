eb75d275ad85f44bf0974ceaed05ccbbed94e9d0
MatthewObirek <matthewobirek@gmail.com>
2023-06-30 11:30:20 -0700

    cleansed code, commented code

--- databaseCourse/elements/pl-relax-element/renderer.js
+++ databaseCourse/elements/pl-relax-element/renderer.js
@@ -2,75 +2,21 @@ $(document).ready(function () {
     //* QUESTION SETUP
     const executeRelalg = relalg_bundle.executeRelalg;
     const Relation = relalg_bundle.Relation;
-    // find all relations used in this branch (recursively)
-    const dbSchema = document.getElementById("database");
 
+    // database setup
+    const dbSchema = document.getElementById("database");
     const dbValue = dbSchema.getAttribute("value");
     const dbArray = dbValue.split(";");
-    // database setup
     const dataset = [dbArray.length];
     for (var i = 0; i < dbArray.length; i++){   
         dataset[i] = executeRelalg(dbArray.at(i), {});
     }
-    // const S = executeRelalg(dbArray.at(0), {});
-    // const P = executeRelalg(dbArray.at(1), {});
-    
-    // const collection = document.getElementsByClassName("card-body submission-body");
-    // collection.item(0).append("Hello")
-    
-    //for(elem in dataset) {
-
-    //}
-    //renderDatasSet()
-
-
-
-    
-    console.log(dataset.length)
+    // Creating dropbtn (Schema) tables
     for(var i = 0; i < dataset.length; i++) {
         dbSchema.innerHTML += createSchemaTables(dataset.at(i)._schema)
     }
 
-    
-
-    function createSchemaTables(dataSchema) {
-        // if tables have no rows, means table does not exist
-        if (dataSchema.length === 0) {
-            return;
-        }
-        let schemaView = "<button type='button' onmouseover='openMenu(this)' onClick='updateCodeMirror(\""+dataSchema._relAliases[0]+"\");' class='dropbtn' id='btn-" + dataSchema._relAliases[0] + "'>" + dataSchema._relAliases[0]
-            + "</button> <div class='dropdown-content' id='schema-" + dataSchema._relAliases[0] + "'>"
-
-        for (var i = 0; i < dataSchema._names.length; i++) {
-            let field = "<div classname='submenu' onClick='updateCodeMirror(\""+dataSchema._names[i]+"\");' id='schema-" + dataSchema._relAliases[0] + "'>" + dataSchema._names[i] +", " + dataSchema._types[i] + "</div>"
-            console.log(i)
-            schemaView += (field)
-        }
-
-        schemaView += "</div>"
-        return schemaView
-
-    }
-
-    window.openMenu = function (val) {
-        closeMenus();
-        document.getElementById('schema-' + val.id.slice(4)).classList.toggle('show');
-    }
-
-    // function to close the modals of the schemas, basically hides each field in the modal
-    function closeMenus() {
-        let dropdowns = document.getElementsByClassName('dropdown-content');
-        for (let i = 0; i < dropdowns.length; i++) {
-            if (dropdowns[i].classList.contains('show')) {
-                dropdowns[i].classList.remove('show')
-            }
-        }
-    }
-    window.onclick = function (e) {
-        if (!e.target.matches('.dropbtn')) {
-            closeMenus()
-        }
-    }
+    //* USER INTERACTIONS
 
     // Creates codemirror code block
     var editorInit = $("#RelaXEditor");
@@ -86,41 +32,94 @@ $(document).ready(function () {
             "Ctrl-Enter": executeEditorContents,
         }
     });
-    window.updateCodeMirror = function (data) {
-        updateCodeMirror(data);
-    }
-    // adds functionality for onclick
-    function updateCodeMirror(data){
-        var doc = editor.getDoc(); //gets the information of the editor
-        doc.replaceRange(data, doc.getCursor()); // adds data at position of cursor
-        editor.focus();
-        editor.setCursor(doc.getCursor());
-    }
 
-
+    //* needed elemnents for rendering.
     var treeElm = $("#tree");
     var outputElm = $("#output");
     var errorElm = $("#error");
     var execBtn = $("#execute");
     execBtn.on("click", executeEditorContents);
+    
 
-    // Execute the RelaX Query
-    function executeEditorContents() {
-        var d1 = dataset.at(0)._schema._relAliases.at(0);
-        var d2 = dataset.at(1)._schema._relAliases.at(0);
-        console.log(d1, d2);
-        console.log(editor.getValue());
-        try {
-            const PR = executeRelalg(editor.getValue(), { "S" : dataset[0], "P" : dataset[1]});
-            console.log(PR);
-            treeElm.contents().remove();
-            createOutputTable(PR);
-            treeElm.append(createRecList(PR));
-        } catch (err) {
-            createErrorOutput(err)
+
+    //* FUNCTION DEFINITIONS
+    
+    //* FUNCTIONS - USER ACTIONS
+    // opens Schema list for selected schema.
+    window.openMenu = function (val) {
+        closeMenus();
+        document.getElementById('schema-' + val.id.slice(4)).classList.toggle('show');
+    }
+    // closes menus when mouse is clicked elsewhere
+    window.onclick = function (e) {
+        if (!e.target.matches('.dropbtn')) {
+            closeMenus()
         }
     }
+
+    // function to close the modals of the schemas, basically hides each field in the modal
+    // helper function for above two window methods.
+    function closeMenus() {
+        let dropdowns = document.getElementsByClassName('dropdown-content');
+        for (let i = 0; i < dropdowns.length; i++) {
+            if (dropdowns[i].classList.contains('show')) {
+                dropdowns[i].classList.remove('show')
+            }
+        }
+    }
+
+    // adds functionality for onclick
+    function updateCodeMirror(data){
+        var doc = editor.getDoc(); // gets the information of the editor
+        doc.replaceRange(data, doc.getCursor()); // adds data at position of cursor
+        editor.focus();             // focuses the user on the editor
+        editor.setCursor(doc.getCursor()); // sets the cursor directly after the newly added char
+    }
+    // helper function to allow for HTML calling.
+    window.updateCodeMirror = function (data) {
+        updateCodeMirror(data);
+    }
     
+    // Execute the RelaX Query
+    function executeEditorContents() {
+        //! d1 and d2 cannot be used to replace "S" and "P"
+        //? How can we fix the "S" and "P" issue
+        var d1 = dataset.at(0)._schema._relAliases.at(0);
+        var d2 = dataset.at(1)._schema._relAliases.at(0);
+
+        try {
+            const PR = executeRelalg(editor.getValue(), { "S" : dataset[0], "P" : dataset[1]}); // gets query results
+            treeElm.contents().remove(); // clears Tree previous results
+            createOutputTable(PR); // creates and renders new output table
+            treeElm.append(createRecList(PR)); // creates and renders new tree
+        } catch (err) {
+            createErrorOutput(err); // creates and renders error in event user has incorrect RA query 
+        }
+    }
+0
+
+    //* FUNCTIONS - RENDERING
+    function createSchemaTables(dataSchema) {
+        // Checks if table exists, exits if not
+        if (dataSchema.length === 0) {
+            return;
+        }
+        // Creates the button HTML element for the table; onclick it adds the tablename to Editor, onhover it shows the schema of the table.
+        let schemaView = "<button type='button' onmouseover='openMenu(this)' onClick='updateCodeMirror(\""+dataSchema._relAliases[0]+"\");' class='dropbtn' id='btn-" + dataSchema._relAliases[0] + "'>" + dataSchema._relAliases[0]
+            + "</button> <div class='dropdown-content' id='schema-" + dataSchema._relAliases[0] + "'>"
+
+        // Creates the submenue element for the table, onclick each member of the table will add itself to the editor
+        for (var i = 0; i < dataSchema._names.length; i++) {
+            let field = "<div classname='submenu' onClick='updateCodeMirror(\""+dataSchema._names[i]+"\");' id='schema-" + dataSchema._relAliases[0] + "'>" + dataSchema._names[i] +", " + dataSchema._types[i] + "</div>"
+            schemaView += field;
+        }
+
+        schemaView += "</div>"
+        return schemaView
+
+    }
+
+    // Clears output fields and renders error
     function createErrorOutput(err) {
         errorElm.contents().remove();
         outputElm.contents().remove();
@@ -129,22 +128,23 @@ $(document).ready(function () {
 
     }
 
+    // clears error and table fields and renders new table
     function createOutputTable(output) {
         outputElm.contents().remove();
         errorElm.contents().remove();
 
-        var table = $("<table></table>");
-        table.append(createTableHeader(output.getResult()._schema._names));
-        table.append(createTableRows(output.getResult()._rows));
+        var table = $("<table></table>"); // creates new table element to be filled
+        table.append(createTableHeader(output.getResult()._schema._names)); // creates table headers
+        table.append(createTableRows(output.getResult()._rows)); // fills table rows
         outputElm.append(table);
     }
 
     // Function that creates the table header
-
     function createTableHeader(columns) {
         var header = $("<thead></thead>");
         var headerRow = $("<tr></tr>");
 
+        // reads each header
         for (var i = 0; i < columns.length; i++) {
             var th = $("<th></th>").text(columns[i]);
             headerRow.append(th);
@@ -154,9 +154,9 @@ $(document).ready(function () {
     }
 
     // Function that creates the table rows for a table
-
     function createTableRows(rows) {
         var rowElements = [];
+        // reads each row in rows and adds them to the row element array.
         rows.forEach(function (row) {
             var tr = $("<tr></tr>");
             row.forEach(function (value) {
@@ -168,76 +168,36 @@ $(document).ready(function () {
         return rowElements;
     }
 
+    //* Recursive function that creates the RelaX output Tree
     function createRecList(output){
-        console.log("hello")
-        var container = $("<div style='margin: 5px'></div>");
+        var container = $("<div style='margin: 5px'></div>"); // Creates container holding section of tree
+        var button = $("<div></div>"); // creates first node of this secton tree
 
-        var button = $("<div></div>");
+        // fills node attributes
         button.attr("id", "button-"+output._functionName);
         button.addClass("btn btn-primary exec-button selection");
         button.text(output._codeInfo.text);
-        button.on("click", function() {
-            // Call the predefined function with the input parameter
-            createOutputTable(output);
-        });
+        // allows each node to return the output at that point of execution 
+        button.on("click", function() { createOutputTable(output); });
         
+        // Checks to see if there are 2, 1, or no Children
         if ((output._child != null) && (output._child2 != null)) {
-            container.append(button);
-            
+            container.append(button); // adds the main node
+            // creates flex container.for children - allowing for side by side rendering
             var newContainer = $("<div></div>");
             newContainer.addClass("flex-container")
-
+            
+            // adds the child nodes
             newContainer.append(createRecList(output._child));
             newContainer.append(createRecList(output._child2));
-            container.append(newContainer);
+            container.append(newContainer); // appends flex container to above container
             return container;
-        } else if (output._child != null) {
-            container.append(button);
-            container.append(createRecList(output._child));
-            return container;
+        } else if (output._child != null) { 
+            container.append(button); // adds the main node.
+            container.append(createRecList(output._child)); // adds the child node
+            return container; 
         } else {
-            return container.append(button);
+            return container.append(button); // adds the main node
         } 
     }
-    
-    /*
-
-    let subElm = $(".card-body.submission-body");
-    let correctElm = $(".card-body.answer-body");
-
-    // if "card-body submission-body" is not empty, function takes in the string
-    if (correctElm.length > 0) {
-        
-        let query = correctElm.text();
-        correctElm.append("correct test")
-        console.log(query);
-
-    }
-
-    // if "card-body submission-body" is not empty, function takes in the string
-    if ( subElm.length > 0) {
-
-        //iterate through each element in the array
-        for (var i = 0; i < subElm.length; i++) {
-            subElm[i].append("submission test: ", i);
-        }
-
-    }
-    */
-    
-
 });
-
-/*
-      let schemaView = "<button type='button' onClick='openMenu(this)' class='dropbtn' id='btn-" + dataSchema._relAliases[0] + "'>" + dataSchema._relAliases[0]
-            + "</button> <div class='dropdown-content' id='schema-" + dataSchema._relAliases[0] + "'>"
-
-        for (var i = 0; i < dataSchema._names.length; i++) {
-            let field = "<div classname='submenu' id='schema-" + dataSchema._relAliases[0] + "'>" + dataSchema._names[i] +", " + dataSchema._types[i] + "</div>"
-            console.log(i)
-            schemaView += (field)
-        }
-
-        schemaView += "</div>"
-        */
-
