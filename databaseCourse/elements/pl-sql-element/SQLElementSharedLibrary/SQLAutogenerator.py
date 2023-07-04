6d05f7abf4a8ae1f23cc432b1a7d355c21ef61c0
Finkch <skyler@thealdersons.org>
2023-07-04 12:46:41 -0700

    Added DECIMAL data type to create-style questions

--- databaseCourse/elements/pl-sql-element/SQLElementSharedLibrary/SQLAutogenerator.py
+++ databaseCourse/elements/pl-sql-element/SQLElementSharedLibrary/SQLAutogenerator.py
@@ -67,6 +67,7 @@ def generateCreate(data, difficulty):
         # Handles the text for units
         match columnValues[i]['unit']:
             case 'INTEGER': questionString += ' (an integer)'
+            case 'DECIMAL': questionString += f" (a decimal value with a total of {columnValues[i]['unitOther'].split(',')[0]} digits, {columnValues[i]['unitOther'].split(',')[1]} of which are after the decimal point)"
             case 'CHAR': questionString += f" (a string of exaclty {columnValues[i]['unitOther']} characters)"
             case 'VARCHAR': questionString += f" (a string up to {columnValues[i]['unitOther']} characters)"
             case 'DATE': questionString += ' (DATE)'
