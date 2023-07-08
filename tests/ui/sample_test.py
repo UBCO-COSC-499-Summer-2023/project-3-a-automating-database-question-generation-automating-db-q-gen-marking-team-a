d90c4c1f9e39e8595981a79bce3934088941f306
azipis <andrei_zipis@hotmail.com>
2023-07-07 22:37:57 -0700

    Test #13

--- tests/ui/sample_test.py
+++ tests/ui/sample_test.py
@@ -6,29 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 class TestSample(unittest.TestCase):
 
     def setUp(self):
-        options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-
-        # Set the path to the ChromeDriver executable
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--window-size=1920,1080')
-        chrome_options.add_argument('--disable-infobars')
-        chrome_options.add_argument('--disable-extensions')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-setuid-sandbox')
-        chrome_options.add_argument('--remote-debugging-port=9222')
-        chrome_options.add_argument('--disable-blink-features=BlockCredentialedSubresources')
-        chrome_options.add_argument('--disable-web-security')
-
-        # Use webdriver_manager to manage ChromeDriver
-        chrome_driver_path = ChromeDriverManager().install()
-        self.driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
 
     def testPageName(self):
         baseUrl = 'http://pl:3000/'
