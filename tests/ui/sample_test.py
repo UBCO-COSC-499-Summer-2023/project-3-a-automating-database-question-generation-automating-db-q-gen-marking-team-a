23e45ccb968124819a08d9eee26cff355889df6c
azipis <andrei_zipis@hotmail.com>
2023-07-07 22:41:15 -0700

    Test #14

--- tests/ui/sample_test.py
+++ tests/ui/sample_test.py
@@ -1,12 +1,31 @@
+import os
+import shutil
 import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 class TestSample(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+
+        # Specify the directory where the ChromeDriver binary will be moved
+        chromedriver_dir = '/usr/local/bin'
+
+        # Set the path to the ChromeDriver executable
+        chromedriver_path = '/usr/bin/chromedriver'
+
+        # Move the ChromeDriver binary to the specified directory
+        shutil.move(chromedriver_path, chromedriver_dir)
+
+        # Set the permissions of the ChromeDriver binary
+        os.chmod(os.path.join(chromedriver_dir, 'chromedriver'), 0o755)
+
+        # Now the ChromeDriver binary should be in a directory in the system's PATH
+        self.driver = webdriver.Chrome(options=options)
 
     def testPageName(self):
         baseUrl = 'http://pl:3000/'
