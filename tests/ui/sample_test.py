d1e1f9dd58854516c55483a000ab1884e3d7ec8f
azipis <andrei_zipis@hotmail.com>
2023-07-07 22:48:49 -0700

    Test #15

--- tests/ui/sample_test.py
+++ tests/ui/sample_test.py
@@ -1,22 +1,7 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-class TestSample(unittest.TestCase):
-
-    def setUp(self):
-
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-
-
-    def testPageName(self):
-        baseUrl = 'http://pl:3000/'
-        driver = self.driver
-        driver.get(baseUrl)
-        assert 'PrairieLearn' in driver.title
-        print(driver.title)
-
-    def tearDown(self):
-        self.driver.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.page_load_strategy = 'normal'
+driver = webdriver.Chrome(options=options)
+driver.get("http://www.google.com")
+driver.quit()
