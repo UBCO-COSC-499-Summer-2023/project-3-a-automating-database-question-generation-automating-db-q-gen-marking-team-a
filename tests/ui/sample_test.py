b4b2628720725ccc2f5411df942b28a971e7b0e6
azipis <andrei_zipis@hotmail.com>
2023-07-08 23:45:11 -0700

    New test #3

--- tests/ui/sample_test.py
+++ tests/ui/sample_test.py
@@ -1,7 +1,38 @@
+import unittest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-options = Options()
-options.page_load_strategy = 'normal'
-driver = webdriver.Chrome(options=options)
-driver.get("http://www.google.com")
-driver.quit()
+from selenium.webdriver.support.ui import WebDriverWait
+
+# example uiTestFile to show how you can automate ui tests, configure the ui test classes/files 
+# and tests
+class TestSample(unittest.TestCase):
+
+    def setUp(self):
+        options = Options()
+        options.add_argument('--headless')
+        self.driver = webdriver.Chrome(options=options)
+
+    # tests that the page loaded ,when PL docker container is up, has title which contains
+    # "PrairieLearn" in it
+    def testPageName(self):
+        baseUrl = 'http://localhost:3000/'
+        driver = self.driver
+        driver.get(baseUrl)
+        assert 'PrairieLearn' in self.driver.title
+        print(self.driver.title)
+    
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# options = Options()
+# options.page_load_strategy = 'normal'
+# driver = webdriver.Chrome(options=options)
+# driver.get("http://www.google.com")
+# driver.quit()
