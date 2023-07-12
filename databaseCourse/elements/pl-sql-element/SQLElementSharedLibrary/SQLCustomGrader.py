61e7087d1a21d37a20e8426484a4484d33f2c0b9
nishant-sr <nishant.srinivasan236@gmail.com>
2023-07-12 09:55:36 -0700

    sqlite3 db interaction working now need to add grading functionality

--- databaseCourse/elements/pl-sql-element/SQLElementSharedLibrary/SQLCustomGrader.py
+++ databaseCourse/elements/pl-sql-element/SQLElementSharedLibrary/SQLCustomGrader.py
@@ -1,4 +1,6 @@
 from difflib import SequenceMatcher
+import sqlite3
+import os
 
 # Uses Python's SequenceMatcher library to check the
 # similarity between the correct answer and the student's
@@ -13,8 +15,25 @@ def customGrader(data):
     # Strips both of whitespace
     stripSA = submittedAnswer.strip()
     stripCA = correctAnswer.strip()
-    
-    # Normalize and split the strings into lists of words
+
+    if os.path.exists("ans.db"):
+        os.remove("ans.db")
+    con = sqlite3.connect("ans.db")
+    cur  = con.cursor()
+    # print(data['params']['db_initialize'])
+    commands = data['params']['db_initialize'].replace('\n', '').replace('\t', '')
+    # print(commands)
+    cur.executescript(commands)
+    con.commit()
+    expectedCode = correctAnswer.replace('\n', ' ').replace('\t', ' ')
+    # print("formatted correct ans",expectedCode)
+    # print("correctans",correctAnswer)
+    expectedAns = cur.execute(expectedCode)
+    print("solution:",expectedAns.fetchall())
+    studentCode = submittedAnswer.replace('\n', ' ').replace('\t', ' ')
+    actualAns = cur.execute(studentCode)
+    print("submitted",actualAns.fetchall())
+    # # Normalize and split the strings into lists of words
     wordsSA = sorted(stripSA.split())
     wordsCA = sorted(stripCA.split())
     
