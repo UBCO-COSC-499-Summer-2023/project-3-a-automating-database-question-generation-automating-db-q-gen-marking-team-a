25ef1e4d6bb9ce97d80f2575186838cddd8492ec
azipis <andrei_zipis@hotmail.com>
2023-07-11 21:16:23 -0700

    New add.

--- tests/ui/sample_test.py
+++ tests/ui/sample_test.py
@@ -8,6 +8,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 import chromedriver_binary
 
 # example uiTestFile to show how you can automate ui tests, configure the ui test classes/files 
@@ -17,8 +19,7 @@ class TestSqlEditor(unittest.TestCase):
     def setUp(self):
         options = Options()
         options.add_argument('--headless')
-        options.add_argument(f'--executable-path=/usr/bin/chromedriver')  # Specify the chromedriver location
-        self.driver = webdriver.Chrome(options=options)
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
     # tests that the home page of PL loads properly
     def testPrairieLearnLoad(self):
@@ -45,8 +46,8 @@ class TestSqlEditor(unittest.TestCase):
         )
         schema.click()
         # 6 | click | id=btn-" + tableName + " | 
-        tableName = WebDriverWait(self.driver,10).until(
-            EC.element_to_be_clickable((By.ID, "btn-\" + tableName + \""))
+        tableName = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "btn-" + tableName))
         )
         tableName.click()
 
@@ -69,8 +70,8 @@ class TestSqlEditor(unittest.TestCase):
         schema.click()
         # 6 | click | xpath=//span[@onclick="addColumnToEditor('customer', 'cid')"] | 
         # self.driver.find_element(By.XPATH, "//span[@onclick=\"addColumnToEditor(\'customer\', \'cid\')\"]").click()
-        schemaCol = WebDriverWait(self.driver,10).until(
-            EC._element_if_visible((By.XPATH, "//span[@onclick=\"addColumnToEditor(\'customer\', \'cid\')\"]"))
+        schemaCol = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[@onclick=\"addColumnToEditor('customer', 'cid')\"]"))
         )
         schemaCol.click()
     
