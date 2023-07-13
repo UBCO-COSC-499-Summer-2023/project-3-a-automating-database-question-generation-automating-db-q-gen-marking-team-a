f20127b77a80140f43230eec98c3b7ae3ba1a029
nishant-sr <nishant.srinivasan236@gmail.com>
2023-07-13 16:13:26 -0700

    grading by column and row count for now

--- databaseCourse/elements/pl-sql-element/SQLElementSharedLibrary/SQLCustomGrader.py
+++ databaseCourse/elements/pl-sql-element/SQLElementSharedLibrary/SQLCustomGrader.py
@@ -16,26 +16,31 @@ def customGrader(data):
     stripSA = submittedAnswer.strip()
     stripCA = correctAnswer.strip()
 
-    if os.path.exists("ans.db"):
-        os.remove("ans.db")
-    con = sqlite3.connect("ans.db")
-    cur  = con.cursor()
-    # print(data['params']['db_initialize'])
-    commands = data['params']['db_initialize'].replace('\n', '').replace('\t', '')
-    # print(commands)
-    cur.executescript(commands)
-    con.commit()
-    expectedCode = correctAnswer.replace('\n', ' ').replace('\t', ' ')
-    # print("formatted correct ans",expectedCode)
-    # print("correctans",correctAnswer)
-    expectedAns = cur.execute(expectedCode)
-    print("solution:",expectedAns.fetchall())
-    studentCode = submittedAnswer.replace('\n', ' ').replace('\t', ' ')
-    actualAns = cur.execute(studentCode)
-    print("submitted",actualAns.fetchall())
     # # Normalize and split the strings into lists of words
     wordsSA = sorted(stripSA.split())
     wordsCA = sorted(stripCA.split())
+
+    questionType = data["params"]["html_params"]["questionType"]
+
+    # partial grading for lab 2 - Create Questions
+    if(questionType == "create"):
+        gradeCreateQuestion(data,correctAnswer,submittedAnswer)
+    
+    # partial grading for lab 2 - Insert Questions
+    if(questionType == "insert"):
+        gradeInsertQuestion(data,correctAnswer,submittedAnswer)
+
+    # partial grading for lab 2 - Update Questions
+    if(questionType == "update"):
+        gradeUpdateQuestion(data,correctAnswer,submittedAnswer)
+
+    # partial grading for lab 2 - Delete Questions
+    if(questionType == "delete"):
+        gradeDeleteQuestion(data,correctAnswer,submittedAnswer)
+
+    # partial grading for lab 2 - Create Questions
+    if(questionType == "query"):
+        gradeQueryQuestion(data,correctAnswer,submittedAnswer)
     
     # Uses the sequence checker to check similatiry between
     # the submission and answer. It returns a number between
@@ -65,3 +70,47 @@ def customGrader(data):
 # 0 means the strings are entirely different.
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
+
+def gradeQueryQuestion(data,correctAnswer,submittedAnswer):
+    expectedTotal = 0
+    actualTotal = 0
+    if os.path.exists("ans.db"):
+        os.remove("ans.db")
+    con = sqlite3.connect("ans.db")
+    cur  = con.cursor()
+    # print(data['params']['db_initialize'])
+    commands = data['params']['db_initialize'].replace('\n', '').replace('\t', '')
+    # print(commands)
+    cur.executescript(commands)
+    con.commit()
+    expectedCode = correctAnswer.replace('\n', ' ').replace('\t', ' ')
+    # print("formatted correct ans",expectedCode)
+    # print("correctans",correctAnswer)
+    expectedAns = cur.execute(expectedCode).fetchall()
+    # print("solution:",expectedAns)
+    studentCode = submittedAnswer.replace('\n', ' ').replace('\t', ' ')
+    actualAns = cur.execute(studentCode).fetchall()
+    # print("submitted",actualAns)
+
+    expectedRowCount = len(expectedAns)
+    expectedColumnCount = len((expectedAns[0]))
+    expectedTotal += expectedRowCount + expectedColumnCount
+    print(expectedTotal,expectedColumnCount,expectedRowCount)
+
+    actualRowCount = len(actualAns)
+    actualColumnCount = len((actualAns[0]))
+    actualTotal += actualRowCount + actualColumnCount
+    print(actualTotal,actualColumnCount,actualRowCount)
+
+    colrowGrade = abs(expectedTotal - actualTotal)
+
+    print((expectedTotal - colrowGrade)/expectedTotal)
+
+def gradeCreateQuestion(data,correctAnswer,submittedAnswer):
+    return 1
+def gradeInsertQuestion(data,correctAnswer,submittedAnswer):
+    return 1
+def gradeUpdateQuestion(data,correctAnswer,submittedAnswer):
+    return 1
+def gradeDeleteQuestion(data,correctAnswer,submittedAnswer):
+    return 1
