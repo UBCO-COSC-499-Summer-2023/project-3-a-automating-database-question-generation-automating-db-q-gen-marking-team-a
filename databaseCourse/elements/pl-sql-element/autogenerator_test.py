0d26bc6f7785f7e4506834253df7b9a0422a29d1
Finkch <skyler@thealdersons.org>
2023-07-17 18:27:58 -0700

    Added empty key to autogen tests
    The value of the dictionary can be empty, it just needs the key to be present

--- databaseCourse/elements/pl-sql-element/autogenerator_test.py
+++ databaseCourse/elements/pl-sql-element/autogenerator_test.py
@@ -38,7 +38,7 @@ class AutogenerateTest(unittest.TestCase):
         initialAns = "\n"
         db_initalize = ""
         data = {'params':{'html_params':{'questionType':testType,'difficulty':difficulty, 'columns': 5, 'joins': 0},
-                          'db_initialize':db_initalize, 'html_table_clauses': {}},
+                          'db_initialize':db_initalize, 'html_table_clauses': {}, 'html_query_clauses':{}},
                 'correct_answers':{'SQLEditor': initialAns}}
         
         autogenerate(data)
