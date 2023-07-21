b8fc09a39eddcee32ee98d8049dcf12572e050ce
MatthewObirek <matthewobirek@gmail.com>
2023-07-21 09:37:34 -0700

    Question autogen beginnings

--- databaseCourse/elements/pl-relax-element/RelaXElementSharedLibrary/RelaXAutogenerator.py
+++ databaseCourse/elements/pl-relax-element/RelaXElementSharedLibrary/RelaXAutogenerator.py
@@ -25,3 +25,52 @@ def autogenerate(data):
 
     # Loads the database into the data variable
     database.loadDatabase(data)
+
+class Question:
+    Query = "π "
+
+    selectStatement = "σ"
+    ClauseArray = { "∧", "∨", "¬", "=", "≠", "≥", "≤", ">", "<"}
+
+    orderByStatement = "τ"
+    groupByStatement = " γ"
+
+    crossJoin = "⨯"
+    naturalJoin = "⨝"
+    outerRightJoins="⟖"
+    outerLeftJoins="⟕"
+    outerFullJoins="⟗"
+    semiRightJoins="⋉"
+    semiLeftJoins="⋊"
+    antiJoins="▷"
+
+    def  __init__(self, dataset, attribDict,
+                    numClauses=0,
+                    orderBy=False,
+                    groupBy=False,
+                    numCrossJoins=0,
+                    numNaturalJoins=0,
+                    numOuterRightJoins=0,
+                    numOuterLeftJoins=0,
+                    numOuterFullJoins=0,
+                    numSemiRightJoins=0,
+                    numSemiLeftJoins=0,
+                    numAntiJoins=0) -> None:
+        
+        attribDict
+
+
+
+        self.numClauses=numClauses
+        self.orderBy=orderBy
+        self.groupBy=groupBy
+        self.numCrossJoins=numCrossJoins
+        self.numNaturalJoins=numNaturalJoins
+        self.numOuterRightJoins=numOuterRightJoins
+        self.numOuterLeftJoins=numOuterLeftJoins
+        self.numOuterFullJoins=numOuterFullJoins
+        self.numSemiRightJoins=numSemiRightJoins
+        self.numSemiLeftJoins=numSemiLeftJoins
+        self.numAntiJoins=numAntiJoins
+        
+        return self
