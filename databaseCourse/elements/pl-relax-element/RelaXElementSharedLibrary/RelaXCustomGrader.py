23d409ec968dc283744300032834c0cf70e4cb6a
azipis <andrei_zipis@hotmail.com>
2023-07-20 23:27:48 -0700

    Changed relaxcustomgrader.

--- databaseCourse/elements/pl-relax-element/RelaXElementSharedLibrary/RelaXCustomGrader.py
+++ databaseCourse/elements/pl-relax-element/RelaXElementSharedLibrary/RelaXCustomGrader.py
@@ -1,7 +1,5 @@
 from difflib import SequenceMatcher
 import requests
-from relational_algebra import Relation, Selection, Projection, CrossProduct, ThetaJoin
-from relational_algebra.formulas.Formula import Formula
 
 
 # Uses Python's SequenceMatcher library to check the
@@ -9,21 +7,61 @@ from relational_algebra.formulas.Formula import Formula
 # submitted answer.
 def customGrader(data):
     
-    host_ip = "142.231.78.234"
-    url = f"http://{host_ip}:4000/index"
-    data = {"Car": "BMW", "Testing": "API"}
+    try:
+        db = data['params']['database']
+        submittedAnswer = data['submitted_answers']['RelaXEditor']
+        correctAnswer = data['correct_answers']['RelaXEditor']
 
-    request = requests.post(url,data=data)
-    print(request.status_code)
-    print(request.text)
-  
-    #execQuery()
+        host_ip = "192.168.1.66"
+        url = f"http://{host_ip}:4000/index"
+
+        # Construct the data to be sent in the POST request
+        data_to_send = {
+            "db": db,
+            "submittedAnswer": submittedAnswer,
+            "correctAnswer": correctAnswer
+        }
+
+        response = requests.post(url, data=data_to_send)
+
+        # Assuming the server returns JSON data containing queriedSA and queriedCA
+        response_data = response.json()
+
+        queriedSA = response_data.get('queriedSA', None)
+        queriedCA = response_data.get('queriedCA', None)
+
+        print("Response status code:", response.status_code)
+        print("Response text:", response.text)
+        print("QueriedSA:", queriedSA)
+        print("QueriedCA:", queriedCA)
+
+    except KeyError as e:
+        # Handle the case when the required keys are missing in the 'data' dictionary
+        print("KeyError:", e)
+        return 0.0
+    except ValueError as e:
+        # Handle the case when the response does not contain valid JSON data
+        print("ValueError:", e)
+        return 0.0
     
-    # Grabs the student answer from data
-    submittedAnswer = data['submitted_answers']['RelaXEditor']
+    # db = data['params']['database']
+    
+    # # Grabs the student answer from data
+    # submittedAnswer = data['submitted_answers']['RelaXEditor']
 
-    # Grabs the solution from data
-    correctAnswer = data['correct_answers']['RelaXEditor']
+    # # Grabs the solution from data
+    # correctAnswer = data['correct_answers']['RelaXEditor']
+    
+    
+    # host_ip = "192.168.1.66"
+    # url = f"http://{host_ip}:4000/index"
+    # data = {"Car": "BMW", "Testing": "API"}
+
+    # request = requests.post(url,data=data)
+    # print(request.status_code)
+    # print(request.text)
+    
+
     
     # Strips both of whitespace
     stripSA = submittedAnswer.strip()
@@ -61,32 +99,3 @@ def customGrader(data):
 # 0 means the strings are entirely different.
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
-
-def evaluate_ra_query(query, sql_con=None):
-    pass
-
-# Example usage:
-def execQuery():
-    # Create relations R and S (same as previous example)
-    relation_R = Relation(name="R")
-    relation_R.add_attributes(["a", "b", "c"])
-    relation_R.add_rows([
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-    ])
-
-    relation_S = Relation(name="S")
-    relation_S.add_attributes(["b", "c", "d"])
-    relation_S.add_rows([
-        [2, 3, 4],
-        [5, 6, 7],
-        [8, 9, 10],
-    ])
-
-    # Evaluate the query
-    #result = evaluate_ra_query(query, sql_con=None)
-
-    # Display the result
-    print("Attributes:", relation_R.attributes)
-    print("Rows:", relation_R.rows)
