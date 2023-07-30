25fa48ee2efdde3c401db4dbaca2d5f98bd4fac9
Finkch <skyler@thealdersons.org>
2023-07-30 16:03:37 -0700

    Added test for parameterized query question

--- databaseCourse/elements/pl-sql-element/autogenerator_test.py
+++ databaseCourse/elements/pl-sql-element/autogenerator_test.py
@@ -531,6 +531,81 @@ class ParameterizedDeleteTests(unittest.TestCase):
             self.assertGreater(len(data['params']['db_initialize_insert_frontend']), 0)
             self.assertGreater(len(data['params']['db_initialize_insert_backend']), 0)
 
+    # Parameterized UPDATE Tests------------------------------------------------------------------------------------------------------------------
+class ParameterizedQueryTests(unittest.TestCase):
+    
+    # Describes how many times each test should be run.
+    # Since we're testing random generation, we need
+    # a sufficient sample size to catch edge cases
+    sampleSize = 1
+
+    # Declares and sets defaults
+    data = {
+        'params': {
+            'html_params': {
+                'random': True,
+                'questionType': 'query',
+                'difficulty': None,
+                'maxGrade': 3,
+                'markerFeedback': True,
+                'expectedOutput': False
+            }
+        },
+        'correct_answers': {
+            'SQLEditor': ''
+        }
+    }
+
+    # Parameters for CREATE to test various cases
+    @parameterized.expand([
+            # columns, joins, conditional, useSubquery, columnsToSelect, orderBy, groupBy, having, limit, isDistinct
+            [data, 5, 2, 2, False, 5, 1, 1, 1, 5, True, sampleSize],   # A bit of everything
+            ])
+    
+    def testParameterizedQuery(self,data,columns,joins,conditionals,subquery,columnsToSelect,orderBy,groupBy,having,limit,isDistinct,sampleSize):        
+        data['params']['html_params']['columns'] = columns
+        data['params']['html_params']['joins'] = joins
+
+        data['params']['html_query_clauses'] = {
+            'conditional': conditionals,
+            'useSubquery': subquery,
+            'columnsToSelect': columnsToSelect,
+            'orderBy': orderBy,
+            'groupBy': groupBy,
+            'having': having,
+            'limit': limit,
+            'with': 0,
+            'isDistinct': isDistinct
+        }
+
+
+        for i in range(sampleSize):
+
+            # Sets values to be empty
+            data['params']['questionString'] = ''
+            data['params']['db_initialize_create'] = ''
+            data['params']['db_initialize_insert_frontend'] = ''
+            data['params']['db_initialize_insert_backend'] = ''
+            data['correct_answers']['SQLEditor'] = ''
+
+            autogenerate(data)
+
+            self.assertIn("SELECT",data['correct_answers']['SQLEditor'])
+            self.assertNotIn("UPDATE",data['correct_answers']['SQLEditor'])
+            self.assertNotIn("CREATE",data['correct_answers']['SQLEditor'])
+            self.assertNotIn("INSERT",data['correct_answers']['SQLEditor'])
+            self.assertNotIn("DELETE",data['correct_answers']['SQLEditor'])
+
+            if subquery or conditionals:
+                self.assertIn("WHERE",data['correct_answers']['SQLEditor'])
+            else:
+                self.assertNotIn("WHERE",data['correct_answers']['SQLEditor'])
+
+            self.assertGreater(len(data['params']['questionString']), 0)
+            self.assertGreater(len(data['params']['db_initialize_create']), 0)
+            self.assertGreater(len(data['params']['db_initialize_insert_frontend']), 0)
+            self.assertGreater(len(data['params']['db_initialize_insert_backend']), 0)
+
         
     # helper functions Tests------------------------------------------------------------------------------------------------------------------
 class HelperFnsTest(unittest.TestCase):
