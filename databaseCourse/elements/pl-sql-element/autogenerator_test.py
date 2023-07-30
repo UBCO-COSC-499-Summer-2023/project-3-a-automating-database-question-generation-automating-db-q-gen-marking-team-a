b5e48dc9897de4d3f86e9f7c96bb6b10201ed9ca
Finkch <skyler@thealdersons.org>
2023-07-30 16:07:50 -0700

    Added more subquery tests

--- databaseCourse/elements/pl-sql-element/autogenerator_test.py
+++ databaseCourse/elements/pl-sql-element/autogenerator_test.py
@@ -560,6 +560,11 @@ class ParameterizedQueryTests(unittest.TestCase):
     @parameterized.expand([
             # columns, joins, conditional, useSubquery, columnsToSelect, orderBy, groupBy, having, limit, isDistinct
             [data, 5, 2, 2, False, 5, 1, 1, 1, 5, True, sampleSize],   # A bit of everything
+            [data, 5, 2, 2, True, 5, 1, 1, 1, 5, True, sampleSize],   # A bit of everything, and a subquery
+            [data, 3, 1, 0, False, 0, 0, 0, 0, 0, False, sampleSize],   # Min, without subquery
+            [data, 3, 1, 0, True, 0, 0, 0, 0, 0, False, sampleSize],   # Min, with subquery
+            [data, 10, 4, 4, False, 10, 4, 4, 4, 5, False, sampleSize], # Max, without subquery
+            [data, 10, 4, 4, True, 10, 4, 4, 4, 5, False, sampleSize],    # Max, with subquery
             ])
     
     def testParameterizedQuery(self,data,columns,joins,conditionals,subquery,columnsToSelect,orderBy,groupBy,having,limit,isDistinct,sampleSize):        
