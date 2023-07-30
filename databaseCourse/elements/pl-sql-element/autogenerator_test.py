9a02cb4bbad10526ece48bf5197f6022174ee727
Finkch <skyler@thealdersons.org>
2023-07-30 14:08:08 -0700

    Added test for parameterized INSERT

--- databaseCourse/elements/pl-sql-element/autogenerator_test.py
+++ databaseCourse/elements/pl-sql-element/autogenerator_test.py
@@ -319,6 +319,66 @@ class ParameterizedCreateTests(unittest.TestCase):
             self.assertEqual(len(data['params']['db_initialize_insert_frontend']), 0)
             self.assertEqual(len(data['params']['db_initialize_insert_backend']), 0)
 
+    # Parameterized INSERT Tests------------------------------------------------------------------------------------------------------------------
+class ParameterizedInsertTests(unittest.TestCase):
+    
+    # Describes how many times each test should be run.
+    # Since we're testing random generation, we need
+    # a sufficient sample size to catch edge cases
+    sampleSize = 5
+
+    # Declares and sets defaults
+    data = {
+        'params': {
+            'html_params': {
+                'random': True,
+                'questionType': 'insert',
+                'difficulty': None,
+                'maxGrade': 3,
+                'markerFeedback': True,
+                'expectedOutput': False
+            }
+        },
+        'correct_answers': {
+            'SQLEditor': ''
+        }
+    }
+
+    # Parameters for CREATE to test various cases
+    @parameterized.expand([
+            # columns, joins
+            [data, 5, 2, sampleSize],  # A bit of everything
+            [data, 3, 0, sampleSize],  # Minimum values
+            [data, 7, 7, sampleSize]  # Max values
+            ])
+    
+    def testParameterizedCreate(self,data,columns,joins,sampleSize):        
+        data['params']['html_params']['columns'] = columns
+        data['params']['html_params']['joins'] = joins
+
+        for i in range(sampleSize):
+
+            # Sets values to be empty
+            data['params']['questionString'] = ''
+            data['params']['db_initialize_create'] = ''
+            data['params']['db_initialize_insert_frontend'] = ''
+            data['params']['db_initialize_insert_backend'] = ''
+            data['correct_answers']['SQLEditor'] = ''
+
+            autogenerate(data)
+
+            self.assertIn("INSERT",data['correct_answers']['SQLEditor'])
+            self.assertNotIn("WHERE",data['correct_answers']['SQLEditor'])
+            self.assertNotIn("DELETE",data['correct_answers']['SQLEditor'])
+            self.assertNotIn("CREATE",data['correct_answers']['SQLEditor'])
+            self.assertNotIn("UPDATE",data['correct_answers']['SQLEditor'])
+            self.assertNotIn("SELECT",data['correct_answers']['SQLEditor'])
+
+            self.assertGreater(len(data['params']['questionString']), 0)
+            self.assertGreater(len(data['params']['db_initialize_create']), 0)
+            self.assertGreater(len(data['params']['db_initialize_insert_frontend']), 0)
+            self.assertGreater(len(data['params']['db_initialize_insert_backend']), 0)
+
         
     # helper functions Tests------------------------------------------------------------------------------------------------------------------
 class HelperFnsTest(unittest.TestCase):
