39cf743766c615c384cf829ceeadfce09b562d65
Finkch <skyler@thealdersons.org>
2023-08-08 16:26:52 -0700

    New test case for query miss slowdown
    Same as the slowest but without conditionals

--- databaseCourse/elements/pl-sql-element/autogenerator_test.py
+++ databaseCourse/elements/pl-sql-element/autogenerator_test.py
@@ -561,6 +561,7 @@ class ParameterizedQueryTests(unittest.TestCase):
     # 3: 15s
     # 4: 15s
     # 5: 59s
+    # 6: 2m 20s
 
 
     # Parameters for QUERY to test various cases
@@ -571,7 +572,8 @@ class ParameterizedQueryTests(unittest.TestCase):
             #[data, 3, 1, 0, False, 0, 0, 0, 0, 0, False, False, sampleSize],   # Min, without subquery
             #[data, 3, 1, 0, True, 0, 0, 0, 0, 0, False, False, sampleSize],   # Min, with subquery
             #[data, 10, 4, 4, False, 10, 4, 4, 4, 5, False, True, sampleSize], # Max, without subquery
-            [data, 10, 4, 4, True, 10, 4, 4, 4, 5, False, True, sampleSize]    # Max, with subquery
+            #[data, 10, 4, 4, True, 10, 4, 4, 4, 5, False, True, sampleSize]    # Max, with subquery
+            [data, 10, 4, 0, True, 10, 4, 4, 0, 5, False, True, sampleSize]
             ])
     
     def testParameterizedQuery(self,data,columns,joins,conditionals,subquery,columnsToSelect,orderBy,groupBy,having,limit,isDistinct,useQueryFunctions,sampleSize):        
